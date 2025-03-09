4cf5b9f4bd6a29ca037c4ea0fa8d13635d2488ac
Abhishek Kumar <abhishek9661342993@gmail.com>
2025-03-09 07:50:39 +0530

    test Deployments 1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
+import vitePluginCopy from 'vite-plugin-copy';
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), 
+    tailwindcss(),
+    vitePluginCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: 'dist/'
+        }
+      ]
+    })
+  ],
 
   server:{
     proxy: {
@@ -14,8 +25,6 @@ export default defineConfig({
 
 
   },
-
-
 
   theme: {
     extend: {
